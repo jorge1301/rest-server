cd846a514ffec0a1def074c22d5e47ee713c22e4
Jorge <jorge13011@gmail.com>
2019-06-14 18:34:15 -0500

    Google Sign In

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
-
+const path = require('path');
 if (process.env.NODE_ENV == 'development') {
     process.env.MONGODB_URI = process.env.MONGODB_URI_LOCAL
     process.env.SEED = process.env.SEED_LOCAL
@@ -14,6 +14,9 @@ if (process.env.NODE_ENV == 'development') {
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+
+//habilitar carpeta public
+app.use(express.static(path.resolve(__dirname , '../public')));
 
 //Llamado de routes
 app.use(require('./routes/index'));
